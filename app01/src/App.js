2afdfa64a1e2844c99e61e335f8ad368e0cd2df5
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-09-08 15:23:18 -0300

    react

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './componentes/Header.js'
+import Corpo from './componentes/Corpo.js'
 
 export default function App() {
     return (
         <>
           <Header/>
+          <Corpo/>
         </>
     );
 }
---
+++ app01/src/componentes/Corpo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Corpo() {
+    return (
+        <header>
+          <h1>Ola Mundo</h1>
+        </header>
+    )
+}
