d9e07f7ab573a9ed298484e8b590122f27c5977f
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-09-24 13:52:01 -0300

    react

--- app01/src/App.js
+++ app01/src/App.js
@@ -2,14 +2,28 @@ import React, { useState } from "react";
 
 export default function App() {
 
-  localStorage.setItem("nome","Lucas")
-  localStorage.getItem("nome")
-  localStorage.removeItem("nome")
+  const [nome,setNome] = useState()
+
+  const adicionar = () => {
+    localStorage.setItem("nome", "Bruno")
+  }
+
+  const VerNome = () => {
+    localStorage.getItem("nome")
+    alert()
+  }
+
+  const remover = () => {
+    localStorage.removeItem("nome")
+  }
 
   return (
 
     <>
-
+      <input type="text" ></input>
+      <button onClick={adicionar}>adicionar</button>
+      <button onClick={VerNome}>VerNome</button>
+      <button onClick={remover}>remover</button>
     </>
 
   )
