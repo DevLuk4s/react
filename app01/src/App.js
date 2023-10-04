eed636c9495b42d404b94f5441f0d443b18776ff
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-04 20:19:35 -0300

    react

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,30 +1,5 @@
 import React, { useState } from "react";
 
 export default function App() {
-
-  const [nome,setNome] = useState()
-
-  const adicionar = (chave,valor) => {
-    localStorage.setItem(chave, valor)
-  }
-
-  const VerNome = (chave) => {
-    alert(localStorage.getItem(chave))
-  }
-
-  const remover = (chave) => {
-    localStorage.removeItem(chave)
-  }
-
-  return (
-
-    <>
-      <label>Digite um nome</label><br />
-      <input type="text" value={nome} onChange={(e) => setNome(e.target.value)}></input>
-      <button onClick={() => adicionar("is_nome", nome)}>adicionar</button>
-      <button onClick={() => VerNome("is_nome")}>Ver Nome</button>
-      <button onClick={() => remover("is_nome")}>remover Nome</button>
-    </>
-
-  )
+  return <></>;
 }
