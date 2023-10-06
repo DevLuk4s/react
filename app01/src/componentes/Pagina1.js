9d53c3fec477e93d5e2af59d03bd152be73a7b02
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-05 22:07:03 -0300

    react

---
+++ app01/src/componentes/Pagina1.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Pagina3(){
+    return(
+    <>
+      <h1>CfbCursos</h1>
+      <p>Teste</p>
+    </>
+    )
+}
---
+++ app01/src/componentes/Pagina3.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Pagina1(){
+    return(
+    <>
+      <h1>CfbCursos</h1>
+      <p>Teste</p>
+    </>
+    )
+}
